93d9261fbccfeea5ffb92e0d12c5ee7f5f65fb4f
AndreiTirpescu <andrei.tirpescu@gmail.com>
2018-05-28 21:14:42 +0300

    added subjects to csv

--- starter_project/api/app/export.js
+++ starter_project/api/app/export.js
@@ -1,9 +1,10 @@
 module.exports = (() => {
     'use strict';
 
-    const {getGroups} = require('./group_actions');
-    const {getRooms} = require('./room_actions');
-    const {getUsers} = require('./user_actions.js');
+    const {getGroups}   = require('./group_actions');
+    const {getRooms}    = require('./room_actions');
+    const {getUsers}    = require('./user_actions.js');
+    const {getSubjects} = require('./subject_actions.js');
 
     var result = {
         csvStr : ""
@@ -11,19 +12,16 @@ module.exports = (() => {
 
     const CSV_SEPARATOR = ';';
 
-    function addField(csvString, fieldValue)
-    {
+    function addField(csvString, fieldValue) {
         csvString.csvStr += fieldValue;
         csvString.csvStr += CSV_SEPARATOR;
     }
 
-    function endFieldLine(csvString)
-    {
+    function endFieldLine(csvString) {
         csvString.csvStr += '\n';
     }
 
-    function addSection(name, arr)
-    {
+    function addSection(name, arr) {
         addField(result, name);
         endFieldLine(result);
         endFieldLine(result);
@@ -36,8 +34,7 @@ module.exports = (() => {
         endFieldLine(result);
     }
 
-    function addDataFromDb(dbRes, dbId)
-    {
+    function addDataFromDb(dbRes, dbId) {
         for(var i = 0; i < dbRes.length; i++)
         {
             for(var j = 0; j < dbId.length; j++)
@@ -50,8 +47,7 @@ module.exports = (() => {
         endFieldLine(result);
     }
 
-    function buildCSV(groups, rooms, users)
-    {
+    function buildCSV(groups, rooms, users, subjects) {
         addSection("Grupe", ["nume", "capacitate"]);
         addDataFromDb(groups, ["name", "number"]);
 
@@ -60,6 +56,17 @@ module.exports = (() => {
 
         addSection("Utilizatori", ["nume", "user", "e-mail"]);
         addDataFromDb(users, ["name", "userName", "mail"]);
+
+        addSection("Subiecte", ["nume", "prescurtare", "data", "frecventa"]);
+        addDataFromDb(subjects, ["name", "short", "date", "frequency"]);
+    }
+
+    function sendCsv(res) {
+        // res.setHeader('Content-type', "application/force-download");
+        // res.setHeader('Content-disposition', 
+        //               'attachment;filename=db_export.csv');
+
+        res.send( result.csvStr );
     }
 
     const exportDb = (req, res) => {
@@ -68,15 +75,18 @@ module.exports = (() => {
             getRooms().then((rooms) => {
                 
                 getUsers().then((users) => {
-                
-                    buildCSV(groups, rooms, users);
                     
-                    res.setHeader('Content-type', "application/force-download");
-                    res.setHeader('Content-disposition', 
-                                  'attachment;filename=db_export.csv');
+                    getSubjects().then((subjects) => {
+                        
+                        buildCSV(groups, rooms, users, subjects);
 
-                    res.send( result.csvStr );
-    
+
+                        sendCsv(res);
+
+                    }).catch((e) => {
+                        console.log(e);
+                    });
+
                 }).catch((e) => {
                     console.log(e);
                 });
