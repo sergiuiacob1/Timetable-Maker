b56d312f1adc87d55b77152175f5954c57cbc8dc
Andrei <casuneanu.andrei97@gmail.com>
2018-05-28 13:20:12 +0300

    css now loads before js

--- starter_project/front-end/webpack.config.js
+++ starter_project/front-end/webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const fs = require('fs');
 
 function generateHtmlPlugins (templateDir) {
@@ -56,7 +56,7 @@ module.exports = {
       {
         test: /\.less$/,
         use: [{
-          loader: 'style-loader' // creates style nodes from JS strings
+          loader: MiniCssExtractPlugin.loader //'style-loader' // creates style nodes from JS strings
         }, {
           loader: 'css-loader' // translates CSS into CommonJS
         }, {
@@ -66,6 +66,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+    }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
