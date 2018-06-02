8763b1781021d5a67ba50f0a1cdb98e6ae9b5533
AndreiTirpescu <andrei.tirpescu@gmail.com>
2018-06-02 16:41:51 +0300

    Fixed a bug where get constraitns for id would return all constraints

--- starter_project/api/app/constraints_routes.js
+++ starter_project/api/app/constraints_routes.js
@@ -1,46 +1,52 @@
 module.exports = (() => {
-    'use strict';
-  
-    const {getConstraints, addConstraint, deleteConstraint} = require('./constraints_actions.js');
+  'use strict';
 
-    const deleteConstraintRoute = (req, res) => {
-      const id = req.body.id;
-      deleteConstraint({id}).then((constraint) => {
-        console.log (constraint);
-        res.json({success: true, message: "Deleted constraint"});
-      }).catch((e) => {
-        console.log(e);
-        res.json({success: false});
-      });
-    };
-  
-    const getConstraintsRoute = (req, res) => {
-      const userId = req.decoded.user.id;
-      getConstraints({userId}).then((constraints) => {
-        console.log(constraints);
-        res.json({success: true, constraints});
-      }).catch((e) => {
-        console.log(e);
-        res.json({success: false});
-      });
+  const {getConstraints, addConstraint, deleteConstraint} = require('./constraints_actions.js');
 
-    };
+  const deleteConstraintRoute = (req, res) => {
+    const id = req.body.id;
+    deleteConstraint({id}).then((constraint) => {
+      console.log (constraint);
+      res.json({success: true, message: "Deleted constraint"});
+    }).catch((e) => {
+      console.log(e);
+      res.json({success: false});
+    });
+  };
 
-    const newConstraintRoute = (req, res) => {
-      const userId = req.decoded.user.id;
-      req.body.userId = userId;
+  const getConstraintsRoute = (req, res) => {
+    const userId = parseInt(req.headers["id"]);
 
-      addConstraint(req.body).then(() => {
-        res.json({success: true, message: "Added constraint"});
-      }).catch((e) => {
-        console.log(e);
-        res.json({success: false});
-      });
-    };
-  
-    return {
-      getConstraintsRoute,
-      newConstraintRoute,
-      deleteConstraintRoute
-    };
-  })();
+    if(isNaN(userId)) {
+      res.json({success: false});
+      return;
+    }
+
+    getConstraints({userId}).then((constraints) => {
+      console.log(constraints);
+      res.json({success: true, constraints});
+    }).catch((e) => {
+      console.log(e);
+      res.json({success: false});
+    });
+
+  };
+
+  const newConstraintRoute = (req, res) => {
+    const userId = req.decoded.user.id;
+    req.body.userId = userId;
+
+    addConstraint(req.body).then(() => {
+      res.json({success: true, message: "Added constraint"});
+    }).catch((e) => {
+      console.log(e);
+      res.json({success: false});
+    });
+  };
+
+  return {
+    getConstraintsRoute,
+    newConstraintRoute,
+    deleteConstraintRoute
+  };
+})();
